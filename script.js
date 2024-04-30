7c00f02dbb658b59fee06f87e556389be8d56ed5
Jade A <120065528+Jade010@users.noreply.github.com>
2024-04-30 01:14:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     setupTypewriter();
-    setupSmoothScrolling();
 });
 
 function setupTypewriter() {
     const element = document.getElementById('typewriter-text');
-    const phrases = ["Jade Aidoghie", "Welcome to my portfolio!"];
+    const phrases = ["Welcome to my portfolio!"];
     let currentPhrase = 0;
     let letterPos = 0;
     let isDeleting = false;
@@ -18,7 +17,7 @@ function setupTypewriter() {
             letterPos++;
         }
 
-        element.innerHTML = fullText.substring(0, letterPos);
+        element.innerHTML = fullText.substring(0, letterPos) + '<span aria-hidden="true"></span>';
 
         if (!isDeleting && letterPos === fullText.length) {
             setTimeout(() => { isDeleting = true; }, 2000);
@@ -26,68 +25,10 @@ function setupTypewriter() {
             isDeleting = false;
             currentPhrase = (currentPhrase + 1) % phrases.length;
             setTimeout(type, 500);
-            return;
         }
 
         setTimeout(type, isDeleting ? 100 : 150);
     }
 
     type();
-}
-
-
-// When the user scrolls down 20px from the top of the document, slide down the navbar
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-    document.getElementById("nav").style.top = "0";
-  } else {
-    document.getElementById("nav").style.top = "-70px";
-  }
-}
-
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("each-project");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
-
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    console.log(element.className, arr2[i]);
-    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
-  }
-}
-
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);     
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("button-section");
-var btns = btnContainer.getElementsByClassName("filter-button");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-    console.log(current);
-  });
 }
