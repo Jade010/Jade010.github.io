65d4e257e8fe4247ffb020b61339cf8d1bb94004
Jade Aidoghie <120065528+Jade010@users.noreply.github.com>
2024-05-20 20:29:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var typewriterText = [
-        "I'm a data analyst and developer.",
-        "I'm passionate about creative design and streamlining processes.",
-        "I design practical solutions for everyday challenges, turning data into direction."
-    ];
-    let textArrayIndex = 0;
-    let charIndex = 0;
-    const typedText = document.getElementById('typewriter');
-
-    function type() {
-        if (charIndex < typewriterText[textArrayIndex].length) {
-            typedText.textContent += typewriterText[textArrayIndex].charAt(charIndex);
-            charIndex++;
-            setTimeout(type, 50);
-        } else {
-            setTimeout(erase, 2000);
-        }
-    }
-
-    function erase() {
-        if (charIndex > 0) {
-            typedText.textContent = typewriterText[textArrayIndex].substring(0, charIndex-1);
-            charIndex--;
-            setTimeout(erase, 60);
-        } else {
-            textArrayIndex = (textArrayIndex + 1) % typewriterText.length;
-            setTimeout(type, 1100);
-        }
-    }
-
-    setTimeout(type, 250);
-});
-
-document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('.filter-button');
     const projects = document.querySelectorAll('.project-card');
 
