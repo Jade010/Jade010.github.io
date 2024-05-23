5dbbb508ddd522736531b1517f55e62ee84e7aab
Jade Aidoghie <120065528+Jade010@users.noreply.github.com>
2024-05-23 11:30:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -30,16 +30,19 @@ document.addEventListener('DOMContentLoaded', function() {
         maxZoom: 18,
     }).addTo(map);
 
-    
-    
+    // Create popup contents
+    var pierceContent = '<div style="display: flex; align-items: center;"><img src="piercecollegelogo.png" alt="Pierce College Logo" style="width:50px;height:50px;margin-right:10px;"><div><b>Pierce College Fort Steilacoom, Lakewood, WA</b><br>Associate of Arts (AA) in Pre-Nursing<br>June 2020</div></div>';
+    var wsuContent = '<div style="display: flex; align-items: center;"><img src="Washington_State_Cougars_logo.png" alt="Washington State University Logo" style="width:50px;height:50px;margin-right:10px;"><div><b>Washington State University, Pullman, WA</b><br>Bachelor of Science (B.S.) in Data Analytics, Minor in Business<br>May 2024</div></div>';
+
     var piercePopup = L.popup({ closeButton: false })
         .setLatLng([47.1717, -122.5185])
-        .setContent('<div><img src="piercecollegelogo.png" alt="Pierce College Logo" style="width:50px;height:50px;"><br><b>Pierce College Fort Steilacoom, Lakewood, WA</b><br>Associate of Arts (AA) in Pre-Nursing<br>June 2020</div>')
-        .openOn(map);
+        .setContent(pierceContent);
 
     var wsuPopup = L.popup({ closeButton: false })
         .setLatLng([46.7298, -117.1817])
-        .setContent('<div><img src="Washington_State_Cougars_logo.png" alt="Washington State University Logo" style="width:50px;height:50px;"><br><b>Washington State University, Pullman, WA</b><br>Bachelor of Science (B.S.) in Data Analytics, Minor in Business<br>May 2024</div>')
-        .openOn(map);
+        .setContent(wsuContent);
+
+    map.addLayer(piercePopup);
+    map.addLayer(wsuPopup);
 });
 
