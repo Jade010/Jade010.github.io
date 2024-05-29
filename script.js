354748401ef4e98e69f1abb09a7df82b4b62bbfb
Jade Aidoghie <120065528+Jade010@users.noreply.github.com>
2024-05-28 20:30:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -23,11 +23,9 @@ document.addEventListener('DOMContentLoaded', function() {
             projects.forEach(project => {
                 // Checking if the project has a tag or if the filter is on all
                 if (filter === 'all' || project.getAttribute('data-tags').includes(filter)) {
-                    project.style.visibility = 'visible';
-                    project.style.opacity = '1';
+                    project.style.display = 'block';
                 } else {
-                    project.style.visibility = 'hidden';
-                    project.style.opacity = '0';
+                    project.style.display = 'none';
                 }
             });
         });
